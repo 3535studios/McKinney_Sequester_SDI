a7bd87a92d15ad994ecffe16ede1f6a457262e7b
Sequester McKinney <quest@3535studios.com>
2015-04-16 22:59:07 -0400

    Setup prompts
    Setup prompts to gather user input.

--- McKinney_Sequester_Expressions_Assignment/js/script.js
+++ McKinney_Sequester_Expressions_Assignment/js/script.js
@@ -7,5 +7,14 @@
 
 //formula A = 2(wl+hl+hw)
 
-//get the length, width, and height from users.
+//get the length, width, and height from users
+
+//enter the length value
+var length = prompt("This will calculate the surface area of a right rectangular prism"\n + "Please enter length value.");
+
+//enter the width value
+var width = prompt("Please enter width value.");
+
+//enter the height value
+var height= prompt("Please enter height value.");
 
