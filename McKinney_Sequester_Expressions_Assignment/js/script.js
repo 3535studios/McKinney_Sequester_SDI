96544cabc200f775f2de5dfee894c9f95ec11e1e
Sequester McKinney <quest@3535studios.com>
2015-04-16 23:07:43 -0400

    Establish formula to calculate area.

--- McKinney_Sequester_Expressions_Assignment/js/script.js
+++ McKinney_Sequester_Expressions_Assignment/js/script.js
@@ -10,11 +10,14 @@
 //get the length, width, and height from users
 
 //enter the length value
-var length = prompt("This will calculate the surface area of a right rectangular prism"\n + "Please enter length value.");
+var length = prompt("This will calculate the surface area of a right rectangular prism \n" + "Please enter the length.");
 
 //enter the width value
-var width = prompt("Please enter width value.");
+var width = prompt("Please enter the width.");
 
 //enter the height value
-var height= prompt("Please enter height value.");
+var height= prompt("Please enter the height.");
+
+//calcuates the surface area of the right rectangular prism
+var area = 2(length, width, height);
 
