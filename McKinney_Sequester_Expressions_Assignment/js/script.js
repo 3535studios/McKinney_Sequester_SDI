2b3017a7cfb1c07d0153131ae0b7efe541977f62
Sequester McKinney <quest@3535studios.com>
2015-04-16 22:52:11 -0400

    Calculate the Surface Area
    This will take 3 inputs to calculate the surface area of a right
    rectangular prism.

---
+++ McKinney_Sequester_Expressions_Assignment/js/script.js
@@ -0,0 +1,11 @@
+/* 	Name: Sequester McKinney
+	Date: 4/16/15
+	Assignment: Expressions Assignment
+*/
+
+//calculate the surface area of a Right rectangular prism
+
+//formula A = 2(wl+hl+hw)
+
+//first step is to get the length, width, and height
+
