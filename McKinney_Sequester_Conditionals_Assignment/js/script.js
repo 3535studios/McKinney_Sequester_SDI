5e1ddf82e08dbc5162cda76f372f729e5d8db0da
Sequester McKinney <quest@3535studios.com>
2015-04-23 16:53:43 -0400

    Setup prompts and tested simple calculations.
    The prompts are in place now I need to setup the conditionals.

--- McKinney_Sequester_Conditionals_Assignment/js/script.js
+++ McKinney_Sequester_Conditionals_Assignment/js/script.js
@@ -9,40 +9,27 @@
 
 //get the base length, base width, pyramid height then divide by 3
 
+//enter the base length
+var length = prompt("Please enter the length");
+
+//enter the base width
+var width = prompt("Please enter the width");
+
+//enter the pryamid height
+var height = prompt("Please enter the height");
+
 //the constant base is 3
 const base = 3;
 
-//enter the base length
-var length = prompt("This will calculate the volume of a right rectangular pyramid \n" + "Please enter the length.");
+var volume = length * width * height / base;
 
-	//enter the length value and if the field is blank the prompt will ask for the length value
-length = (length == '' || 'null') ? prompt("This field can't be blank.  Please enter the length.") : prompt("Please enter the width");
 
-//enter the base width
-var width = prompt("Please enter the width.");
-
-if (width != '' || 'null') {
-	//the pyramid height
-	var height = prompt("Please enter the height.");
-} else if (height == '' || 'null') {
-	var height = prompt("This field can't be blank. Please enter the height.");
-} else {
-	//calculates the volume of a right rectangular pyramid
-	var volume = length * width * height / base;
-}
-//validates that all fields have values
-if (length == ''|| 'null' || width == ''|| 'null' || height == ''|| 'null') {
-	console.log("This caluclation will not be correct because one of the fields is blank.  Please refresh the page and try again.")
-} else {
-	//prints out the values the user entered and displays the final results.
+//prints out the values the user entered and displays the final results.
 console.log("You entered the length "+length+" You entered the width "+ width+" You entered the height "+height+" The volume of a right rectangular pyramid is "+volume);
-}
 
-
-
+//validates that all fields have values
+(length === '' || width === '' || height === '') ? console.log("This caluclation will not be correct because at least one of the fields is blank.  Please refresh the page and try again.") : volumeCalc = length * width * height / base;
 
 /*	I typed the values of 4, 6, and 7 and the result was 56.  This is correct after verifying */
 
 /* I typed the values of 5, 6, and 8 and the result was 236.  This is correct after verifying */
-
-
