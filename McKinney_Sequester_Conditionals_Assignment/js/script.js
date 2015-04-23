7716ab889ac66374a4a94012c14ad960bbb2ef9d
Sequester McKinney <quest@3535studios.com>
2015-04-23 19:15:08 -0400

    Setup && and else if conditonals

--- McKinney_Sequester_Conditionals_Assignment/js/script.js
+++ McKinney_Sequester_Conditionals_Assignment/js/script.js
@@ -11,10 +11,12 @@
 
 //enter the base length
 var length = prompt("Please enter the length");
+
+//validates if the length value is blank.
 if (length === "") {
 	length = prompt("This field is blank.\n Please enter the length.")
 } else {
-	console.log("The value of this field "+length);
+	console.log("The length you entered is "+length);
 }
 
 //enter the base width
@@ -24,13 +26,13 @@ var width = prompt("Please enter the width");
 if (width === "") {
 	width = prompt("This field is blank.\n Please enter the width.")
 } else {
-	console.log("The value of this field "+width);
+	console.log("The width you entered is "+width);
 }
-//enter the pryamid height
+//enter the pyramid height
 var height = prompt("Please enter the height");
 
-//validates if the height value is blank.
-(height === "") ? height = prompt("This field is blank.\n Please enter the height.") : console.log("The value of this field "+height)
+//validates if the height value is blank using ternary conditional
+(height === "") ? height = prompt("This field is blank.\n Please enter the height.") : console.log("The height you entered is "+height)
 
 //the constant base is 3
 const base = 3;
@@ -38,10 +40,23 @@ const base = 3;
 //calculates the volume of a right rectangular pyramid
 var volume = length * width * height / base;
 
+//conditionals to determine what values have entered into the prompts
+if(length === "5" && width === "6" && height === "8") {
+	console.log("You have entered all the values to get a volume of 80.");
 
-//prints out the values the user entered and displays the final results.
-console.log("You entered the length "+length+" You entered the width "+ width+" You entered the height "+height+" The volume of a right rectangular pyramid is "+volume);
+	//prints out the values the user entered and displays the final results.
+	console.log("You entered the length "+length+" You entered the width "+ width+" You entered the height "+height+" The volume of a right rectangular pyramid is "+volume);
+} else if (length === "4" || width === "6" || height === "7") {
+	console.log("You have entered all the values to get a volume of 56.");
+	//prints out the values the user entered and displays the final results.
+	console.log("You entered the length "+length+" You entered the width "+ width+" You entered the height "+height+" The volume of a right rectangular pyramid is "+volume);
+} else {
+	//prints out the values the user entered and displays the final results.
+	console.log("You entered the length "+length+" You entered the width "+ width+" You entered the height "+height+" The volume of a right rectangular pyramid is "+volume);
+}
 
 /*	I typed the values of 4, 6, and 7 and the result was 56.  This is correct after verifying */
 
-/* I typed the values of 5, 6, and 8 and the result was 236.  This is correct after verifying */
+/* I typed the values of 5, 6, and 8 and the result was 80.  This is correct after verifying */
+
+/* I typed the values of 2, 3, and 4 and the result was 8.  This is correct after verifying */
