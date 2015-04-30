ae5bbcb1a3ad6d26a8a2d0fe6fd1b1c5dcff32b3
Sequester McKinney <quest@3535studios.com>
2015-04-30 13:54:10 -0400

    Created new functions folder for assignment.
    Setup functions assignment with new folder

---
+++ McKinney_Sequester_Functions_Assignment/js/script.js
@@ -0,0 +1,17 @@
+/* 	Name: Sequester McKinney
+	Date: 4/30/15
+	Assignment: Functions Assignment
+*/
+
+//calculate compound annual growth rate for investments
+
+//formula ((current value/initial value)^(1/n))-1
+
+
+	 
+
+/*	I typed the values of 4, 6, and 7 and the result was 56.  This is correct after verifying */
+
+/* I typed the values of 5, 6, and 8 and the result was 80.  This is correct after verifying */
+
+/* I typed the values of 2, 3, and 4 and the result was 8.  This is correct after verifying */
