dc1476ef79f33c0f4e276421056844e63e7d13d7
Sequester McKinney <quest@3535studios.com>
2015-04-30 23:12:26 -0400

    validation is working properly.  moving to function.

--- McKinney_Sequester_Functions_Assignment/js/script.js
+++ McKinney_Sequester_Functions_Assignment/js/script.js
@@ -12,7 +12,7 @@ var currentValue = prompt("Please enter the current value of your investment");
 
 // while loop to validate the current value prompt
 while (currentValue == "") {
-	console.log("This field can not be blank. \n Please enter the current value of your investment.")
+	currentValue = prompt("This field can not be blank. \n Please enter the current value of your investment.")
 }
 
 //enter the initial value of the investment
@@ -20,7 +20,7 @@ var initialValue = prompt("Please enter the initial ");
 
 // while loop to validate the initial value prompt
 while (initialValue == "") {
-	console.log("This field can not be blank. \n Please enter the intial value of your investment.")
+	initialValue = prompt ("This field can not be blank. \n Please enter the intial value of your investment.")
 }
 
 //enter the initial value of the investment
@@ -28,27 +28,27 @@ var numOfYears = prompt("Please enter the number of years you have had the inves
 
 // while loop to validate the number of years prompt
 while (numOfYears == "") {
-	console.log("This field can not be blank. \n Please enter the number years you have had the investment.")
+	numOfYears = prompt("This field can not be blank. \n Please enter the number years you have had the investment.")
 }
 
 // function to perform calculation for cagr
 
 // normal function
-function CAGR(currentValue,initialValue,numOfYears) {
+/* function CAGR(currentValue,initialValue,numOfYears) {
 	// final results of calculation
 	var results = ((currentValue/initialValue)Math.pow(1/numOfYears))-1;
 	return results;
 }
-console.log("The compound annual growth rate on your investment is " + results);
+console.log("The compound annual growth rate on your investment is " + results); */
 
 // anonymous function
-var finalCal = function CAGR(currentValue,initialValue,numOfYears) {
+/* var finalCal = function CAGR(currentValue,initialValue,numOfYears) {
 	// final results of calculation
-	var results = ((currentValue/initialValue)Math.pow(1/numOfYears))-1;
+	var results = ((currentValue/initialValue)exp(1/numOfYears))-1;
  	return results;
 }
 
-console.log("The compound annual growth rate on your investment is " + results);
+console.log("The compound annual growth rate on your investment is " + results); */
 
 
 /*	I typed the values of 5000, 10000, and 12 and the result was 56.  This is correct after verifying */
