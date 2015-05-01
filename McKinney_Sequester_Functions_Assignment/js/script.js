3bcdf80435a9cc85caf56f3794caabfeb265efdc
Sequester McKinney <quest@3535studios.com>
2015-04-30 22:37:25 -0400

    function setup and calculations

--- McKinney_Sequester_Functions_Assignment/js/script.js
+++ McKinney_Sequester_Functions_Assignment/js/script.js
@@ -7,15 +7,43 @@
 
 //formula ((current value/initial value)^(1/n))-1
 
-// while loop to validate each prompt
+//enter the initial value of the investment
+var currentValue = prompt("Please enter the current value of your investment");
+
+// while loop to validate the current value prompt
+while (currentValue == "") {
+	console.log("This field can not be blank. \n Please enter the current value of your investment.")
+}
+
+//enter the initial value of the investment
+var initialValue = prompt("Please enter the initial ");
+
+// while loop to validate the initial value prompt
+while (initialValue == "") {
+	console.log("This field can not be blank. \n Please enter the intial value of your investment.")
+}
+
+//enter the initial value of the investment
+var numOfYears = prompt("Please enter the number of years you have had the investment.");
+
+// while loop to validate the number of years prompt
+while (numOfYears == "") {
+	console.log("This field can not be blank. \n Please enter the number years you have had the investment.")
+}
+
 
 // function to perform calculation for cagr
 
 // normal function
+function CAGR(currentValue,initialValue,numOfYears) {
+	// final results of calculation
+	((currentValue/initialValue)Math.pow(1/numOfYears))-1;
+	
+}
 
 // anonymous function
 
-// final results of calculation
+
 
 
 /*	I typed the values of 4, 6, and 7 and the result was 56.  This is correct after verifying */
